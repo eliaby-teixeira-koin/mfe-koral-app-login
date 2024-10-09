a19d8c5ca401bd7d8a5e333584d0782fc896dd0b
eliabyteixeira <eliaby.teixeira@gmail.com>
2024-10-09 16:17:29 -0300

    create repo

---
+++ src/root.component.tsx
@@ -0,0 +1,12 @@
+// import { Button } from '@koin/util-ui';
+// import { Button } from "@koin/util-ui";
+// import { Button } from "@koin/util-ui";
+
+export default function Root(props) {
+  return (
+    <section>
+      <h1>Siow pelo amor de Deus</h1>
+      {/* <Button>Teste</Button> */}
+    </section>
+  );
+}
